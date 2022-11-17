ea5277d56ca3d74e698c3523fcf4168ac91d009d
+ <+>
2022-11-17 16:48:18 -0500

    commit tree 038cfc32837ab39fe4d36b6203330f58e7cdc1c3

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(value)
+        create(title: value)
+    end
+
+    def self.first_movie
+        first
+    end
+
+    def self.last_movie
+        last
+    end
+
+    def self.movie_count
+        count
+    end
+
+    def self.find_movie_with_id(id)
+        find(id)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        where('release_date > ?', 2002)
+    end
+
+    def update_with_attributes(attributes)
+        update(attributes)
+    end
+
+    def self.update_all_titles(value)
+        update_all(title: value)
+    end
+
+    def self.delete_by_id(id)
+        delete(id)
+    end
+
+    def self.delete_all_movies
+        delete_all
+    end
 end
